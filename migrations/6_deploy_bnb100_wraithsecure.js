1a7d27c0627129455b0972bcb298bc7a2e6bcbe2
Project Wraith <86335670+WraithProtocol@users.noreply.github.com>
2021-08-13 00:15:15 -0500

    Add files via upload

---
+++ migrations/6_deploy_bnb100_wraithsecure.js
@@ -0,0 +1,15 @@
+/* global artifacts */
+const BNBWraith = artifacts.require('BNBWraith')
+const Verifier = artifacts.require('Verifier')
+const hasherContract = artifacts.require('Hasher')
+
+
+module.exports = function(deployer, network, accounts) {
+  return deployer.then(async () => {
+    const verifier = await Verifier.deployed()
+    const hasherInstance = await hasherContract.deployed()
+    await BNBWraith.link(hasherContract, hasherInstance.address)
+    const wraith = await deployer.deploy(BNBWraith, verifier.address, '100000000000000000000', 20, accounts[0])
+    console.log('BNB100 WraithSecure\'s address ', wraith.address)
+  })
+}
